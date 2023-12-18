97da017a813a8138853896c49ef965a32a40514b
Thomas Castleman <contact@draugeros.org>
2023-12-18 17:50:06 -0500

    major update - Added disclosures - Changed format of script so it can have access to parent process's environment and use pre-defined functions

--- 7.7/entry_point.sh
+++ 7.7/entry_point.sh
@@ -21,30 +21,72 @@
 #  MA 02110-1301, USA.
 #  
 #
-set -Ee
-set -o pipefail
-
-### MAIN UPGRADE PROCEDURE ###
-echo -e " - SETTING UP NEW APT SOURCES\n\n\n"
-sed -i 's/jammy/noble/g' /etc/apt/sources.list
-sed -i.save 's/strigoi/nzambi/g' /etc/apt/sources.list
-apt-get update
-
-echo -e "\n\n\n - INITIATING UPGRADE\n\n\n"
+function main ()
 {
-	apt-get -o Dpkg::Options::="--force-confold" --force-yes -y dist-upgrade
-} || {
-    apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install --fix-broken
-	apt-get -o Dpkg::Options::="--force-confold" --force-yes -y autopurge
-	apt-get -o Dpkg::Options::="--force-confold" --force-yes -y dist-upgrade
+	### MAIN UPGRADE PROCEDURE ###
+	echo -e " - SETTING UP NEW APT SOURCES\n\n\n"
+	sudo sed -i 's/jammy/noble/g' /etc/apt/sources.list
+	sudo sed -i.save 's/strigoi/nzambi/g' /etc/apt/sources.list
+	sudo apt-get update
+
+	echo -e "\n\n\n - INITIATING UPGRADE\n\n\n"
+	{
+		sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y dist-upgrade
+	} || {
+		sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install --fix-broken
+		autopurge
+		sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y dist-upgrade
+	}
+
+	echo -e "\n\n\nMAIN UPGRADE COMPLETE\n\n\n"
+	yes_array=("yes Yes YES y Y")
+	no_array=("no No NO n N")
+
+	# KDE Changeover
+	# Wayland Changeover
+
+	# Confirm user is using Pipewire, enforce change over if not
+	# Confirm user is using systemd-boot-manager if on EFI, enforce change over if not
+
+	autopurge
+	sudo apt-get clean
 }
 
-echo -e "\n\n\nMAIN UPGRADE COMPLETE\n\n\n"
-yes_array=("yes Yes YES y Y")
-no_array=("no No NO n N")
+function disclosure ()
+{
+	changes=""
+	added_time=0
+	if [[ -f /usr/bin/pulseaudio ]]; then
+		changes="
 
-# KDE Changeover
-# Wayland Changeover
+$(BOLD)PIPEWIRE$(RESET)
+During the upgrade from Drauger OS 7.5.1, users had the option to upgrade from PulseAudio to Pipewire. Now, with the upgrade from Drauger OS 7.6, PulseAudio is no longer going to be supported. As such, all users will be required to upgrade to Pipewire if they wish to retain support for their OS.
 
-apt-get -o Dpkg::Options::="--force-confold" --force-yes -y autopurge
-apt-get clean
+Pipewire is a next-generation audio interface for software. It spoofs PulseAudio, so all your PulseAudio apps will continue to work as they have before. It also offers lower audio latency, better Bluetooth support, and better audio integration with apps and games running in Wine or Proton."
+		added_time=$((added_time+10))
+	fi
+	if [[ ! -f /usr/bin/systemd-boot-manager ]]; then
+		if [[ -d /boot/efi/EFI/systemd ]]; then
+			changes="$changes
+
+$(BOLD)SYSTEMD-BOOT-MANAGER$(RESET)
+During the upgrade from Drauger OS 7.5.1, users had the option to upgrade from a simple script to handle their bootloader, to the new systemd-boot-manager package. This change only applies to users using UEFI/EFI installations of Drauger OS. We detected you are using one of these installations, so this change may apply to you.
+
+Systemd-Boot-Manager allows easier control of your bootloader, is less error prone, and is capable of receiving updates, unlike the script which was originally being used."
+			added_time=$((added_time+10))
+		fi
+	fi
+
+	if [[ "$changes" == "" ]]; then
+		# None of the mandatory changes apply to this system. Bypass the rest of this function
+		return 1
+	fi
+
+	echo -e "
+\t\t### DISCLOSURE OF ENFORCED CHANGES ###
+
+During the upgrade to the new version of Drauger OS, a few changes will be enforced. If you have no idea what these are, you are safe to move on. However, it is encouraged that all users read the below disclousure to understand the changes that will occur if they agree to them. These changes are mandatory. If you agree to the upgrade, these changes $(BOLD)will$(RESET) occur.$changes
+
+If you installed Drauger OS for the first time with version 7.6, these changes do not apply to you. However, if you upgraded your system from Drauger OS 7.5.1, you may be among the small number of users who are affected by this change."
+	timer $((added_time+10)) "Please read the above disclosure(s)."
+}
